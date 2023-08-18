4b58dabf686788c65333333f4579caeceec8cd8f
Govert Brinkmann <gbrinkmann@beeldengeluid.nl>
2023-08-18 09:47:32 +0000

    Improve vega integration

---
+++ components/VegaView.vue
@@ -0,0 +1,15 @@
+<template>
+   <div id='vis'/>
+</template>
+
+<script setup>
+import embed from 'vega-embed'
+
+const props = defineProps({
+  spec: { type: Object, required: true, default: {} },
+})
+
+onMounted(() => {
+  embed("#vis", props.spec)
+}) 
+</script>
