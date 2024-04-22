7f5bc90010001b64fe7df83fc03b15f13f156975
Govert Brinkmann <gbrinkmann@beeldengeluid.nl>
2024-04-22 14:15:04 +0000

    Add dataset definition to datasets page

---
+++ content/en/dataset-definition.md
@@ -0,0 +1,5 @@
+On this page you find the datasets that Sound & Vision makes available.
+
+We define a _dataset_ as: "A collection of data and/or metadata, grouped according to a logical axis (e.g., thematic, license, etc.), collectively described and published. References to the actual objects may or may not be part of a dataset. The dataset as a whole is provided with additional metadata, such as a title, a description, and a license."
+
+[//]: # (source: https://docs.google.com/document/d/1r0NRtUjCnN9Tj6NV_Qg4NdaXdkSCK42aa5hrrRBx1Yw/edit#bookmark=id.6f5vc5vyj9kx)
--- pages/datasets/index.vue
+++ pages/datasets/index.vue
@@ -1,10 +1,34 @@
 <template>
-  <CardPage
-    :cards="datasetPages"
-    :title="title"
-    :card-path="cardPath"
-    :data-class="dataClass"
-  />
+  <div>
+    <HeaderPage>
+      <!-- SectionHeading -->
+      <template #header>
+        <SectionHeading :title="$t(title)" :data-class="dataClass" />
+      </template>
+
+      <!-- Content -->
+      <template #content>
+        <CardGrid
+          :cards="datasetPages"
+          :path="cardPath"
+          :data-class="dataClass"
+        >
+          <template #header>
+            <v-expansion-panels class="justify-center justify-md-start">
+              <v-expansion-panel class="mb-4">
+                <v-expansion-panel-title class="text-subtitle-2">
+                  {{ $t('defining_datasets') }}
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <ContentRenderer class="nuxt-content" :value="definition" />
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </template>
+        </CardGrid>
+      </template>
+    </HeaderPage>
+  </div>
 </template>
 
 <script setup>
@@ -14,21 +38,41 @@ const dataClass = 'dataset'
 const title = `${dataClass}s`
 const cardPath = `${dataClass}s-slug`
 
-const path = dataClass + 's'
-const { data: pathLocalized } = await useAsyncData(async () => {
-  const content = await queryContent(`${i18n.locale.value}/${path}`)
+const pagesPath = dataClass + 's'
+const { data: pagesPathLocalized } = await useAsyncData(async () => {
+  const content = await queryContent(`${i18n.locale.value}/${pagesPath}`)
     .find()
     .catch(() => {
       // ignore 404s
     })
   const locale =
     content.length > 0 ? i18n.locale.value : i18n.fallbackLocale.value
-  return `${locale}/${path}`
+  return `${locale}/${pagesPath}`
 })
 const { data: datasetPages } = await useAsyncData(async () => {
-  return queryContent(pathLocalized.value)
+  return queryContent(pagesPathLocalized.value)
     .where({ hidden: { $ne: true } })
     .find()
+    .catch(() => {
+      // ignore error of missing page
+    })
+})
+
+const definitionPath = 'dataset-definition'
+const { data: definitionPathLocalized } = await useAsyncData(async () => {
+  const content = await queryContent(`${i18n.locale.value}/${definitionPath}`)
+    .find()
+    .catch(() => {
+      // ignore 404s
+    })
+  const locale =
+    content.length > 0 ? i18n.locale.value : i18n.fallbackLocale.value
+  return `${locale}/${definitionPath}`
+})
+const { data: definition } = await useAsyncData(async () => {
+  return queryContent(definitionPathLocalized.value)
+    .where({ hidden: { $ne: true } })
+    .findOne()
     .catch(() => {
       // ignore error of missing page
     })
