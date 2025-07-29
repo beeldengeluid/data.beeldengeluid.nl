98365883be9194e458ea86ab4612c8252b5ed77e
Govert Brinkmann <gbrinkmann@beeldengeluid.nl>
2025-07-29 09:36:22 +0000

    Fix slug hook

--- server/plugins/content.ts
+++ server/plugins/content.ts
@@ -5,8 +5,8 @@ export default defineNitroPlugin((nitroApp) => {
   // https://github.com/nuxt/content/blob/776e428eb2987e5a8c237fbc90e2d9beb8e3b0e3/packages/content/lib/database.js#L271
   // https://content.nuxtjs.org/api/advanced#contentfileafterparse
   nitroApp.hooks.hook('content:file:afterParse', (file) => {
-    if (file._id.endsWith('.md')) {
-      file.slug = path.basename(file._path)
+    if (file.id.endsWith('.md')) {
+      file.slug = path.basename(file.path)
     }
   })
 })
